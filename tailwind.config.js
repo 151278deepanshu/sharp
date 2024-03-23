9d9600febc6278465c048c14a19b93a59a014e0a
Deepanshu Gupta <deepanshu@Deepanshus-MacBook-Air.local>
2024-03-23 13:28:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors:{
+      blue:{
+          50: '#f0f7ff',
+          100: '#cce4ff',
+          200: '#99c9ff',
+          300: '#66a9ff',
+          400: '#3385ff',
+          500: '#0066ff',
+          600: '#0052cc',
+          700: '#003d99',
+          800: '#002966',
+          900: '#001933',
+          }
+  }
+  },
+  plugins: [],
+}
+
